53ed047ef916633fd4ee53ee81628850df49972b
AkihitoNishimura <plumizreop.3@gmail.com>
2020-09-07 08:44:41 +0900

    Initial Commit

---
+++ AnimeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimeApp
+//
+//  Created by 西村映人 on 2020/09/07.
+//  Copyright © 2020 西村映人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
