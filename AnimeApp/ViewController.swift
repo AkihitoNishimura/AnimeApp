4e8a131b3843be05798b6214295393fd17221abc
AkihitoNishimura <plumizreop.3@gmail.com>
2020-09-07 09:51:40 +0900

    firat commit

--- AnimeApp/ViewController.swift
+++ AnimeApp/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var animationImage: UIImageView!
+
+//    画像データ格納用の配列を作成
+    var images : Array<UIImage> = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        // 画像を配列に格納
+        while let attackImage = UIImage(named: "attak\(images.count+1)") {
+            images.append(attackImage)
+        }
+        
+        
     }
 
-
+    
+    
+    @IBAction func actionButton(_ sender: Any) {
+        // アニメーションの適用
+        animationImage.animationImages = images
+        // 1.5秒間隔
+        animationImage.animationDuration = 1.5
+        // 1回繰り返し
+        animationImage.animationRepeatCount = 1
+        // アニメーションを開始
+        animationImage.startAnimating()
+    }
+    
 }
 
